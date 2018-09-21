c4a5e15d231f91d628836a78ea4318986145a88b
mauryarohit002 <36223794+mauryarohit002@users.noreply.github.com>
2018-09-21 11:28:13 +0530

    Add files via upload

--- php/strings.php
+++ php/strings.php
@@ -2,28 +2,24 @@
 $x="iT iS a heAder.";
 echo $x;
 echo "<br>";
-
 echo strlen($x);
 echo "<br>";
-
+echo str_word_count($x);
+echo "<br>";
+echo strrev($x);
+echo "<br>";
+echo strpos($x,"is");
+echo "<br>";
+echo str_replace("is","has",$x);
+echo "<br>";
 echo strtoupper($x);
 echo "<br>";
-
 echo strtolower($x);
 echo "<br>";
-
 echo lcfirst($x);
 echo "<br>";
-
 echo ucfirst($x);
 echo "<br>";
-
 echo ucwords($x);
 echo "<br>";
 
-
-
-
-
-
- ?>
